5af5e491a49b0d3e89c17c76d59cfb7bd9aa85c2
Namratha V <mathewbony908@gmail.com>
2026-03-26 23:54:04 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const { createClient } = require("@supabase/supabase-js");
+
+const supabase = createClient(
+  "https://vnfpmtaunwbeyvhrwvyl.supabase.co",
+  "sb_publishable_E1F74RWWgJFqu4zQb-FX-A_R82Z7t_d"
+);
+
+app.post("/contact", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const { data, error } = await supabase
+    .from("contacts")
+    .insert([{ name, email, message }]);
+
+  if (error) {
+    console.log(error);
+    return res.status(500).send("Error saving data");
+  }
+
+  res.send("Message saved successfully ✅");
+});
+app.listen(5000, () => console.log("Server running on port 5000"));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
---
+++ frontend/script.js
@@ -0,0 +1,48 @@
+// Scroll function
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Contact form
+const form = document.getElementById("form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  const data = { name, email, message };
+
+  console.log(data); // debug
+
+  try {
+    await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    alert("Message sent 🚀");
+    form.reset(); // clears form
+  } catch {
+    alert("Error ❌");
+  }
+});
+// Scroll animation
+const elements = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+
+    if (position < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
